8fd5a25ebf9d52e9dd43ee1470f20e2f749d7909
rcmdnk <rcmdnk@gmail.com>
2013-12-27 13:27:59 +0100

    first import

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+scripts=(https://raw.github.com/rcmdnk/sd_cl/master/etc/sd_cl)
+if [ x"$prefix" = x ];then
+  prefix=/usr/local
+fi
+
+prefix=`echo $prefix|sed 's|--prefix=||'|sed "s|^~|$HOME|"|sed "s|^\.|$(pwd)|"`
+
+echo
+echo "###############################################"
+echo "Install to $prefix/etc"
+echo "###############################################"
+echo
+sudo=""
+if [ -d $prefix/etc ];then
+  touch $prefix/etc/.install.test >& /dev/null
+  if [ $? -ne 0 ];then
+    sudo=sudo
+  else
+    rm -f $prefix/etc/.install.test
+  fi
+else
+  mkdir -p $prefix/etc>&  /dev/null
+  if [ $? -ne 0 ];then
+    sudo mkdir -p $prefix/etc
+    sudo=sudo
+  fi
+fi
+
+for s in ${scripts[@]};do
+  sname=`basename $s`
+  echo Intalling ${sname}...
+  $sudo curl -fsSL -o $prefix/etc/$sname $s
+done
